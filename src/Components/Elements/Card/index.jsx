d811c93a275661901f9d007072d9ea6ea4f0bb8a
AndhikaRizky04 <111202214138@mhs.dinus.ac.id>
2024-11-25 12:46:45 +0700

    fix psk1

--- src/Components/Elements/Card/index.jsx
+++ src/Components/Elements/Card/index.jsx
@@ -1,39 +1,25 @@
-import PropTypes from 'prop-types';
-
 const Card = (props) => {
-  const { title = false, desc, variant } = props;
-
-  return (
-    <div className={`flex flex-col flex-1 mb-6 ${variant}`}>
-      {title && (
-        <>
-          {title.length === 1 ? (
-            <div className="hidden md:block md:text-lg md:text-gray-02 md:mb-4">
-              {title}
-            </div>
-          ) : (
-            <div className="text-lg text-gray-02 mb-4">{title}</div>
-          )}
-        </>
-      )}
-      <div className="bg-white rounded-lg px-6 py-5 shadow-xl flex-1">
-        {desc}
+    const { title = false, desc, variant } = props;
+  
+    return (
+      <div className={`flex flex-col flex-1 mb-6 ${variant}`}>
+        {title && (
+          <>
+            {title.length == 1 ? (
+              <div className="hidden md:block md:text-lg md:text-gray-02 md:mb-4">
+                {title}
+              </div>
+            ) : (
+              <div className="text-lg text-gray-02 mb-4">{title}</div>
+            )}
+          </>
+        )}
+        <div className="bg-white rounded-lg px-6 py-5 shadow-xl flex-1">
+          {desc}
+        </div>
       </div>
-    </div>
-  );
-};
-
-// Menambahkan propTypes
-Card.propTypes = {
-  title: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
-  desc: PropTypes.node.isRequired, // `desc` wajib diisi dan bisa berupa node (teks, elemen, dll.)
-  variant: PropTypes.string, // Bisa berupa string
-};
-
-// Menambahkan defaultProps untuk props opsional
-Card.defaultProps = {
-  title: false,
-  variant: '',
-};
-
-export default Card;
+    );
+  };
+  
+  export default Card;
+  
