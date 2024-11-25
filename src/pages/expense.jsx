42ff6f6de21082e41b8cdc6c08b35efe3e072df2
AndhikaRizky04 <111202214138@mhs.dinus.ac.id>
2024-11-25 13:14:58 +0700

    fix psk v2

--- src/pages/expense.jsx
+++ src/pages/expense.jsx
@@ -1,5 +1,5 @@
 
-import Card from "../components/Elements/Card/index";
+import Card from "../components/Elements/Card";
 import MainLayout from "../components/Layouts/MainLayout";
 
 const ExpensePage = () => {
