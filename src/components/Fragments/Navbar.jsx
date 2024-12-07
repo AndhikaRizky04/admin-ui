69c75225796f247aaae8cc17d1f52336f3419967
AndhikaRizky04 <111202214138@mhs.dinus.ac.id>
2024-12-08 03:45:06 +0700

    fix balance stepper v2

--- src/components/Fragments/Navbar.jsx
+++ src/components/Fragments/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { Icon } from "../Elements/Icon";
 import Logo from "../Elements/Logo";
 const Navbar = () => {
