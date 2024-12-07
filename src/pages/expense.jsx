e6598a310813722613e3f273ecae077fd7fdcafd
AndhikaRizky04 <111202214138@mhs.dinus.ac.id>
2024-12-08 03:41:22 +0700

    fix balance stepper lagi

--- src/pages/expense.jsx
+++ src/pages/expense.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import Card from "../components/Elements/Card";
+import Card from "../components/Elements/Card/index";
 import MainLayout from "../components/Layouts/MainLayout";
 
 const ExpensePage = () => {
--- src/pages/forgotPassword.jsx
+++ src/pages/forgotPassword.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import FormForgotPassword from "../components/Fragments/FormForgotPassword"
 
 const ForgotPassword = () => {
