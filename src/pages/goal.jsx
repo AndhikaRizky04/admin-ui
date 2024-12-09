07e0ef75cc3732fcb512beb472eb60c7d6bf71da
AndhikaRizky04 <111202214138@mhs.dinus.ac.id>
2024-12-09 17:51:43 +0700

    State Management with Context fix new

--- src/pages/goal.jsx
+++ src/pages/goal.jsx
@@ -1,4 +1,4 @@
-import Card from "../Components/Elements/Card";
+import Card from "../components/Elements/Card/index";
 import MainLayout from "../Components/Layouts/MainLayout";
 
 const GoalPage = () => {
