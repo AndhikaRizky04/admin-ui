605914f1979a5ca5413418769b506dcb1dc151f6
AndhikaRizky04 <111202214138@mhs.dinus.ac.id>
2024-12-09 13:40:26 +0700

    fix8

--- src/pages/expense.jsx
+++ src/pages/expense.jsx
@@ -1,4 +1,4 @@
-import Card from "../components/Elements/Card/index"
+import Card from "../components/Elements/Card"
 import MainLayout from "../components/Layouts/MainLayout";
 
 const ExpensePage = () => {
